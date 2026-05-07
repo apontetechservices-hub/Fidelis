a668ca39ff1482719c46083aa18dccbcb1e09978
Chris <apontetechservices@gmail.com>
2026-05-07 17:58:15 -0400

    Update package name to io.github.apontetechservices.fidelis for Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fidelis.fidelis"
+    namespace = "io.github.apontetechservices.fidelis"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.fidelis.fidelis"
+        applicationId = "io.github.apontetechservices.fidelis"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
